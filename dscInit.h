7afe901d49a994a4a431af85c456216997e611ad
jbroesch <78771218+jbroesch@users.noreply.github.com>
2021-07-01 17:04:03 -0700

    Update dscInit.h

--- dscInit.h
+++ dscInit.h
@@ -1,13 +1,18 @@
 /**********************************************************************
-* © James D. Broesch, 2011,2021
+* Â© James D. Broesch, 2011,2021
 *
 * FileName:        dscInit.h
 * Dependencies:    Header (.h) files if applicable, see below
 * Processor:       dsPIC33FJ256GP506
-* Compiler:        MPLAB® C30 v3.00 or higher
+* Compiler:        MPLABÂ® C30 v3.00 or higher
 *
 ************************************************************************/
+
+#ifndef DSCINIT_H
+#define DSCINIT_H
 
 #include <xc.h>
 
 void dscInit(void);
+
+#endif
